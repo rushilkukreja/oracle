476bbf4f4847e3108ac921c9e62e1dc7e5e456c9
Rushil Kukreja <rushilkukreja@gmail.com>
2024-07-19 10:43:54 -0400

    Initial commit
    Added rockets table and all associated files (R code, images, data, etc.). Added initial results for rocket emissions. Added README file.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Open-source Rocket and Constellation Lifecycle Emissions (ORACLE)
+
+Welcome to the Open-source Rocket and Constellation Lifecycle Emissions 
+(`oracle`) repository.
+
+## Team
+- Rushil Kukreja, Thomas Jefferson High School for Science and Technology.
+- Edward Oughton, George Mason University.
---
+++ vis/rockets_tables.r
@@ -0,0 +1,59 @@
+library(ggpubr)
+library(ggplot2)
+library(tidyverse)
+library(ggtext)
+library(gridExtra)
+library(grid)
+library("readxl")
+
+folder <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+rct = read.csv(file.path(folder, '..', 'data', 'raw', "rockets_table.csv"),
+               row.names = 1)
+new_names <- c('Ariane-5', 'Ariane-62', 'Ariane-64', 'Atlas V', 'Falcon-9', 'Falcon-Heavy')
+colnames(rct) <- new_names
+
+filename = 'rockets_table.png'
+folder_tables = file.path(folder, 'figures', 'tables')
+path = file.path(folder_tables, filename)
+dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
+png(
+  path,
+  units = "in",
+  width = 15,
+  height = 3,
+  res = 480
+)
+table_grob <- tableGrob(rct)
+n_col <- ncol(rct)
+for (i in seq_len(n_col)) {
+  table_grob$widths[i] <- unit(0.13, "npc")
+}
+grid.newpage()
+grid.draw(table_grob)
+dev.off()
+
+rct = read.csv(file.path(folder, '..', 'data', 'raw', "rockets_table_2.csv"),
+               row.names = 1)
+new_names <- c('Long March-5', 'LVM3', 'New Glenn', 'Soyuz-FG', 'Starship', 'Vulcan Centaur')
+colnames(rct) <- new_names
+
+filename = 'rockets_table_2.png'
+folder_tables = file.path(folder, 'figures', 'tables')
+path = file.path(folder_tables, filename)
+dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
+png(
+  path,
+  units = "in",
+  width = 15,
+  height = 3,
+  res = 480
+)
+table_grob <- tableGrob(rct)
+n_col <- ncol(rct)
+for (i in seq_len(n_col)) {
+  table_grob$widths[i] <- unit(0.13, "npc")
+}
+grid.newpage()
+grid.draw(table_grob)
+dev.off()
