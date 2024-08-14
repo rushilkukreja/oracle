0f5f15f14d2ce73978c9bc6d62568761ea1643ec
Rushil Kukreja <137130690+rushilkukreja@users.noreply.github.com>
2024-08-14 11:15:17 -0400

    Update parameters tables

--- vis/l_rocket_parameters.r
+++ vis/l_rocket_parameters.r
@@ -8,27 +8,51 @@ library("readxl")
 
 folder <- dirname(rstudioapi::getSourceEditorContext()$path)
 
-rct = read.csv(file.path(folder, '..', 'data', 'raw', "rocket_parameters.csv"),
-               row.names = 1)
+rct <- read.csv(file.path(folder, '..', 'data', 'raw', "rocket_parameters.csv"),
+                row.names = 1)
 new_names <- c('Ariane-5', 'Ariane-62', 'Ariane-64', 'Atlas V', 'Falcon-9', 'Falcon-Heavy', 'Long March-5', 'LVM3', 'New Glenn', 'Soyuz-FG', 'Starship', 'Vulcan Centaur')
 colnames(rct) <- new_names
 
-filename = 'rocket_parameters_table.png'
-folder_tables = file.path(folder, 'figures', 'tables')
-path = file.path(folder_tables, filename)
-dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
+rct_transposed <- t(rct)
+
+rct_first_half <- rct_transposed[, 1:4]
+rct_second_half <- rct_transposed[, 5:ncol(rct_transposed)]
+
+folder_tables <- file.path(folder, 'figures', 'tables')
+dir.create(folder_tables, showWarnings = FALSE, recursive = TRUE)
+
+filename_first = 'rocket_parameters_table_1.png'
+path_first = file.path(folder_tables, filename_first)
 png(
-  path,
+  path_first,
   units = "in",
-  width = 41,
-  height = 4,
+  width = 20,
+  height = 8,
   res = 480
 )
-table_grob <- tableGrob(rct)
-n_col <- ncol(rct)
-for (i in seq_len(n_col)) {
-  table_grob$widths[i] <- unit(0.06, "npc")
+table_grob_first <- tableGrob(rct_first_half)
+for (i in seq_len(ncol(rct_first_half))) {
+  max_width <- max(unit.c(table_grob_first$widths[i], stringWidth(colnames(rct_first_half)[i])))
+  table_grob_first$widths[i] <- max_width
 }
 grid.newpage()
-grid.draw(table_grob)
+grid.draw(table_grob_first)
+dev.off()
+
+filename_second = 'rocket_parameters_table_2.png'
+path_second = file.path(folder_tables, filename_second)
+png(
+  path_second,
+  units = "in",
+  width = 25,
+  height = 8,
+  res = 480
+)
+table_grob_second <- tableGrob(rct_second_half)
+for (i in seq_len(ncol(rct_second_half))) {
+  max_width <- max(unit.c(table_grob_second$widths[i], stringWidth(colnames(rct_second_half)[i])))
+  table_grob_second$widths[i] <- max_width
+}
+grid.newpage()
+grid.draw(table_grob_second)
 dev.off()
--- vis/m_constellation_parameters.R
+++ vis/m_constellation_parameters.R
@@ -10,7 +10,7 @@ folder <- dirname(rstudioapi::getSourceEditorContext()$path)
 
 set_equal_widths <- function(table_grob) {
   n_col <- ncol(table_grob)
-  equal_width <- unit(0.6 / n_col, "npc")
+  equal_width <- unit(0.9 / n_col, "npc")
   for (i in seq_len(n_col)) {
     table_grob$widths[i] <- equal_width
   }
@@ -18,31 +18,17 @@ set_equal_widths <- function(table_grob) {
 }
 
 rct1 = read.csv(file.path(folder, '..', 'data', 'raw', "constellation_parameters.csv"), row.names = 1)
-new_names1 <- c('Astra', 'BlueWalker', 'Cinnamon-937', 'Flock', 'Globalstar', 'Guowang', 'Hanwha', 'Honghu-3', 'HVNET', 'KLEO', 'Kuiper', 'Lacuna')
+new_names1 <- c('Astra', 'BlueWalker', 'Cinnamon-937', 'Flock', 'Globalstar', 'Guowang', 'Hanwha', 'Honghu-3', 'HVNET', 'KLEO', 'Kuiper', 'Lacuna', 'Lightspeed', 'Lynk', 'Omni', 'OneWeb', 'Rassvet', 'Semaphore-C', 'SferaCon', 'Starlink (Gen 2)', 'Swarm', 'Xingshidai', 'Xingwang', 'Yinhe')
 colnames(rct1) <- new_names1
+rct1_transposed <- t(rct1)
 
 filename1 = 'constellation_parameters_table.png'
 folder_tables1 = file.path(folder, 'figures', 'tables')
 path1 = file.path(folder_tables1, filename1)
 dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
-png(path1, units = "in", width = 41, height = 2.5, res = 480)
-table_grob1 <- tableGrob(rct1)
+png(path1, units = "in", width = 10, height = 7.5, res = 480)
+table_grob1 <- tableGrob(rct1_transposed)
 table_grob1 <- set_equal_widths(table_grob1)
 grid.newpage()
 grid.draw(table_grob1)
-dev.off()
-
-rct2 = read.csv(file.path(folder, '..', 'data', 'raw', "constellation_parameters_2.csv"), row.names = 1)
-new_names2 <- c('Lightspeed', 'Lynk', 'Omni', 'OneWeb', 'Rassvet', 'Semaphore-C', 'SferaCon', 'Starlink (Gen 2)', 'Swarm', 'Xingshidai', 'Xingwang', 'Yinhe')
-colnames(rct2) <- new_names2
-
-filename2 = 'constellation_parameters_table_2.png'
-folder_tables2 = file.path(folder, 'figures', 'tables')
-path2 = file.path(folder_tables2, filename2)
-dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
-png(path2, units = "in", width = 41, height = 2.5, res = 480)
-table_grob2 <- tableGrob(rct2)
-table_grob2 <- set_equal_widths(table_grob2)
-grid.newpage()
-grid.draw(table_grob2)
 dev.off()
